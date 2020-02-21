73868f4acc26605e0207ae43cb15ccb927505c49
alkashi <hoyeungw@outlook.com>
2020-02-22 00:15:14 +0800

    fix vo param in deca and deco

--- logger/deco/test/deco.test.js
+++ logger/deco/test/deco.test.js
@@ -1,7 +1,10 @@
-import { deca } from '../index'
+import { deca, deco } from '../index'
 import { Basics, Matrices, Misc, Objects, Vectors } from './candidates'
+import { logger } from '@spare/logger'
 
 const candidates = { ...Basics, ...Vectors, ...Matrices, ...Objects, ...Misc }
 for (let [key, something] of Object.entries(candidates)) {
   `${key}: ${something|> deca({})}`  |> console.log
 }
+
+({ y: 2020, m: 2, d: 21 }) |> deca({ vo: 1 }) |> logger
