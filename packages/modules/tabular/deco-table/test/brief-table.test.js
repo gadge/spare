730a11828a67da165e3ef6023645cbf2fd5ebed7
alkashi <hoyeungw@outlook.com>
2020-03-27 11:21:05 +0800

    patch fix

--- packages/modules/tabular/deco-table/test/brief-table.test.js
+++ packages/modules/tabular/deco-table/test/brief-table.test.js
@@ -1,7 +1,11 @@
 import { TableCollection } from '@foba/table'
 import { says } from '@spare/logger'
 import { Deco } from '../index'
+import { Table } from '@analys/table'
+import { iso } from '@vect/vector-init'
 
-const table = TableCollection.flopShuffle()
+const table = TableCollection.flopShuffle() |>Table.from
 
-table |> Deco({ top: 5, bottom: 3, left: 3, right: 1 }) |> says['table']
+table.pushColumn('chn', iso(table.ht, '五'))
+
+table |> Deco({ top: 5, bottom: 3, left: 3, right: 1, fullAngle: true }) |> says['table']
