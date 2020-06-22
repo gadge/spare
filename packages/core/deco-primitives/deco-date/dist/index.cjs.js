b603bd71498e98287c5b0523a7af752506641b94
hazen <hoyeungw@outlook.com>
2020-06-22 18:40:57 +0800

    update dependencies

--- packages/core/deco-primitives/deco-date/dist/index.cjs.js
+++ packages/core/deco-primitives/deco-date/dist/index.cjs.js
@@ -2,52 +2,11 @@
 
 Object.defineProperty(exports, '__esModule', { value: true });
 
-var presets = require('@palett/presets');
-var projector = require('@palett/projector');
-var enumChars = require('@spare/enum-chars');
+var timestampPretty = require('@valjoux/timestamp-pretty');
 
-const dyeY = projector.Colorant({
-  min: 1990,
-  max: 2030
-}, presets.METRO);
-const dyeM = projector.Colorant({
-  min: 1,
-  max: 12
-}, presets.METRO);
-const dyeD = projector.Colorant({
-  min: 1,
-  max: 31
-}, presets.METRO);
-const dyeh = projector.Colorant({
-  min: 1,
-  max: 24
-}, presets.SUBTLE);
-const dyes = projector.Colorant({
-  min: 1,
-  max: 60
-}, presets.SUBTLE);
-
-const p4 = x => x >= 1000 ? '' + x : x.padStart(4, '0');
-
-const p2 = x => x >= 10 ? '' + x : '0' + x;
-
-const decoDate = date => {
-  var _p, _p2, _p3;
-
-  const Y = date.getFullYear(),
-        M = date.getMonth() + 1,
-        D = date.getDate();
-  return (_p = p4(Y), dyeY(Y)(_p)) + enumChars.DASH + (_p2 = p2(M), dyeM(M)(_p2)) + enumChars.DASH + (_p3 = p2(D), dyeD(D)(_p3));
-};
-const decoTime = date => {
-  var _p4, _p5, _p6;
-
-  const h = date.getHours(),
-        m = date.getMinutes(),
-        s = date.getSeconds();
-  return (_p4 = p2(h), dyeh(h)(_p4)) + enumChars.RT + (_p5 = p2(m), dyes(m)(_p5)) + enumChars.RT + (_p6 = p2(s), dyes(s)(_p6));
-};
-const decoDateTime = date => decoDate(date) + '\'' + decoTime(date);
+const decoDate = timestampPretty.date;
+const decoTime = timestampPretty.roughTime;
+const decoDateTime = timestampPretty.dateTime;
 
 exports.decoDate = decoDate;
 exports.decoDateTime = decoDateTime;
--- packages/core/deco-primitives/deco-date/src/decoDate.js
+++ packages/core/deco-primitives/deco-date/src/decoDate.js
@@ -1,24 +1,7 @@
-import { METRO, SUBTLE } from '@palett/presets'
-import { Colorant }      from '@palett/projector'
-import { DASH, RT }      from '@spare/enum-chars'
+import { date, dateTime, roughTime } from '@valjoux/timestamp-pretty'
 
-const dyeY = Colorant({ min: 1990, max: 2030 }, METRO)
-const dyeM = Colorant({ min: 1, max: 12 }, METRO)
-const dyeD = Colorant({ min: 1, max: 31 }, METRO)
-const dyeh = Colorant({ min: 1, max: 24 }, SUBTLE)
-const dyes = Colorant({ min: 1, max: 60 }, SUBTLE)
+export const decoDate = date
 
-const p4 = x => x >= 1000 ? '' + x : x.padStart(4, '0')
-const p2 = x => x >= 10 ? '' + x : '0' + x
+export const decoTime = roughTime
 
-export const decoDate = date => {
-  const Y = date.getFullYear(), M = date.getMonth() + 1, D = date.getDate()
-  return (p4(Y) |> dyeY(Y)) + DASH + (p2(M) |> dyeM(M)) + DASH + (p2(D) |> dyeD(D))
-}
-
-export const decoTime = date => {
-  const h = date.getHours(), m = date.getMinutes(), s = date.getSeconds()
-  return (p2(h) |> dyeh(h)) + RT + (p2(m) |> dyes(m)) + RT + (p2(s) |> dyes(s))
-}
-
-export const decoDateTime = date => decoDate(date) + '\'' + decoTime(date)
+export const decoDateTime = dateTime
