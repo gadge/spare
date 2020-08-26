9d4175c8c79b00d6cd8eebc1d639c4fdf68f4efe
hazen <hoyeungw@outlook.com>
2020-08-26 11:30:51 +0800

    add @spare/cite

---
+++ packages/core/texting/cite/dist/index.cjs.js
@@ -0,0 +1,14 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+var cite = require('src/cite');
+
+
+
+Object.defineProperty(exports, 'cite', {
+	enumerable: true,
+	get: function () {
+		return cite.cite;
+	}
+});
