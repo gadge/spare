950600ac1f17c1e2e4f90935ce5b14d700632625
hazen <hoyeungw@outlook.com>
2021-03-24 22:06:02 +0800

    add config.js

--- packages/deco-tabulars/deco-crostab/resources/config.js
+++ packages/deco-tabulars/deco-crostab/resources/config.js
@@ -2,7 +2,7 @@ import { decoFlat }  from '@spare/deco-flat'
 import { LF }        from '@spare/enum-chars'
 import { POINTWISE } from '@vect/enum-matrix-directions'
 
-export const CONF_DECO_CROSTAB = {
+export const CONFIG = {
   delim: LF,
   read: decoFlat,
   ansi: true,
