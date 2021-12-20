45f4bdf765481fde6e2f02de023c3822bc514bdf
Hoyeung <hoyeungw@outlook.com>
2021-12-20 10:31:12 +0800

    migrated package.json type to module

--- rollup.config.js
+++ rollup.config.js
@@ -1,47 +1,39 @@
-import babel                      from '@rollup/plugin-babel'
-import commonjs                   from '@rollup/plugin-commonjs'
-import json                       from '@rollup/plugin-json'
-import nodeResolve                from '@rollup/plugin-node-resolve'
-import { decoObject, decoString } from '@spare/logger'
-import fileInfo                   from 'rollup-plugin-fileinfo'
+import { babel }           from '@rollup/plugin-babel'
+import json                from '@rollup/plugin-json'
+import { nodeResolve }     from '@rollup/plugin-node-resolve'
+// import { decoObject, ros } from '@spare/logger'
+// import fileInfo            from 'rollup-plugin-fileinfo'
 
-const { name, dependencies, main, module } = require(process.cwd() + '/package.json')
+const { name, dependencies, exports } = require(process.cwd() + '/package.json')
 
-console.log('Executing', name, decoString(process.cwd()))
-console.log('Dependencies', decoObject(dependencies || {}))
+// console.log(ros('Executing'), name, process.cwd())
+// console.log(ros('Dependencies'), decoObject(dependencies || {}, { bracket: true }))
 
-export default [
-  {
-    input: 'index.js',
-    external: Object.keys(dependencies || {}),
-    output: [
-      { file: main, format: 'cjs' },  // CommonJS (for Node) build.
-      { file: module, format: 'esm' }  // ES module (for bundlers) build.
-    ],
-    plugins: [
-      nodeResolve({ preferBuiltins: true }),
-      commonjs({ include: /node_modules/ }),
-      babel({
-        babelrc: false,
-        comments: true,
-        sourceMap: true,
-        exclude: 'node_modules/**',
-        babelHelpers: 'bundled',
-        presets: [
-          [ '@babel/preset-env', { targets: { node: '14' }, loose: true } ]
-        ],
-        plugins: [
-          // ['@babel/plugin-proposal-decorators', { legacy: true }],
-          // [ '@babel/plugin-proposal-optional-chaining' ],
-          // [ '@babel/plugin-proposal-nullish-coalescing-operator' ],
-          [ '@babel/plugin-proposal-pipeline-operator', { proposal: 'minimal' } ],
-          [ '@babel/plugin-proposal-class-properties', { loose: true } ],
-          // [ '@babel/plugin-proposal-private-methods', { loose: true } ],
-          // [ '@babel/plugin-transform-runtime', { helpers: false, } ]
-        ]
-      }),
-      json(),
-      fileInfo(),
-    ]
-  }
-]
+export default {
+  input: 'index.js',
+  external: Object.keys(dependencies || {}),
+  output: [
+    { file: exports['import'], format: 'esm' },  // ES module (for bundlers) build.
+    { file: exports['require'], format: 'cjs' }  // CommonJS (for Node) build.
+  ],
+  plugins: [
+    nodeResolve({ preferBuiltins: true }),
+    // commonjs({ include: /node_modules/ }),
+    babel({
+      babelrc: false,
+      comments: true,
+      sourceMap: true,
+      exclude: 'node_modules/**',
+      babelHelpers: 'bundled',
+      presets: [
+        [ '@babel/preset-env', { targets: { node: '16' } } ]
+      ],
+      plugins: [
+        [ '@babel/plugin-proposal-pipeline-operator', { proposal: 'minimal' } ],
+      ]
+    }),
+    json(),
+    // fileInfo(),
+  ]
+}
+
