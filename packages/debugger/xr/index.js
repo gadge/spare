962d1acc34afd4fb63b04f534fd1bc3ff4ecf00b
hazen <hoyeungw@outlook.com>
2022-05-03 14:04:27 +0800

    export ros and says

--- packages/debugger/xr/index.js
+++ packages/debugger/xr/index.js
@@ -1,9 +1,12 @@
-import { camelToSnake } from '@spare/phrasing'
-import { ros }          from '@spare/says'
+import { camelToSnake } from '@texting/phrasing'
 import { bracket }      from '@texting/bracket'
+import { hasAnsi }      from '@texting/charset-ansi'
+import { ProxyUtil }    from './src/ProxyUtil'
+import { Roster }       from './src/Roster'
 import { Steno }        from './src/Steno'
 import { Keep }         from './src/TextUtil'
 
+const ansiOrSnake = tx => hasAnsi(tx) ? tx : camelToSnake(tx)
 /**
  *
  * @param {string} [word]
@@ -13,14 +16,18 @@ import { Keep }         from './src/TextUtil'
 export const Xr = Steno.build
 
 export class Singletons {
-  static flat = new Steno()
-  static camel = Steno.build('', Keep.make(x => x|> camelToSnake|> ros |> bracket))
+  static roster = Roster.build() // ITALIC
+  static flat = Steno.build()
+  static camel = Steno.build('', Keep.make(x => x|> ansiOrSnake |> Singletons.roster.get |> bracket))
 }
 
-export const xr = word => Singletons.flat.rebuild(word)
-
-export const cr = word => Singletons.camel.rebuild(word)
-
-export const x = new Proxy(Singletons.flat, { get(steno, key) { return steno.rebuild()[key] } })
-
-export const $ = new Proxy(Singletons.camel, { get(steno, key) { return steno.rebuild()[key] } })
+export const ros = name => Singletons.roster.get(name)
+export const xr = word => Singletons.flat.iso(word)
+export const cr = word => Singletons.camel.iso(word)
+export const x = new Proxy(Singletons.flat, { get(steno, key) { return steno.iso(key) } })
+export const $ = new Proxy(Singletons.camel, { get(steno, key) { return steno.iso(key) } })
+export const says = new Proxy(Singletons.roster, {
+  get(roster, key) {
+    return ProxyUtil.getMethodOrNull(Singletons.camel, key) ?? Singletons.camel.iso(roster.get(key))
+  }
+})
