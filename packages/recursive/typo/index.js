fc1db9917f3337a1377f0dabd3768814c70b1e0f
Hoyeung <hoyeungw@outlook.com>
2022-08-18 09:32:09 +0800

    delete typo

--- packages/recursive/typo/index.js
+++
@@ -1,4 +0,0 @@
-export { Arc }         from './src/Arc.js'
-export { ArcFab }      from './src/ArcFab.js'
-export { Cate }        from './src/Cate.js'
-export { StringBound } from './src/StringBound.js'
--- packages/recursive/typo/src/Cate.js
+++
@@ -1,5 +0,0 @@
-export class Cate {
-  static Num = 0
-  static Str = 1
-  static NaN = 3
-}
